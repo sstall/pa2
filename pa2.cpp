b4735d04ec69b5f3d54411948b1a845980d53e1e
Samuel <samuelstall@ufl.edu>
2017-02-27 19:01:49 -0500

    Added error checking for inputs and started on adding function

--- pa2.cpp
+++ pa2.cpp
@@ -1,9 +1,6 @@
 //pa2.cpp
 //Samuel Stall
 
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
 #include "pa2.h"
 
 using namespace std;
@@ -12,21 +9,79 @@ int main(int argc, char* argv[]) {
 
 	programMode(argc, argv);
 
-	List l;
+	//Initializes Programlist, creating 32 nodes containing "Free" as the data
+	ProgramList l;
 
 	for(int i = 0; i < 32; i++) {
 		l.addNode("Free");
 	}
 
-	l.printList();
+	int userChoice = 0;
+
+	do{
+		userChoice = mainMenu();
+
+		switch(userChoice) {
+			//Add program
+			case 1: {
+				string addData;	
+				int programSize;
+	
+				cout << "Program name - ";
+				cin >> addData;
+				cout << "Program size (KB) - ";
+				programSize = userInputInt();
+				int pagesRequired = ceil((double)programSize / 4);
+
+				if(programSize <= 0) {
+					cout << "Error, invalid memory entry for Program " << addData << endl;
+				}
+				else if(programSize > 128) {
+					cout << "Error, not enough memory for Program " << addData << endl;
+				}
+				else {
+					if(l.addProgram(addData, pagesRequired, argv)) {
+						cout << "Program " << addData << " added successfully: " << pagesRequired << " page(s) used." << endl; 
+					}
+				}
+				break;
+			} 
+			//Kill program
+			case 2: {
+				string removeData;
+
+				cout << "Program name - ";
+				cin >> removeData;
+				
+				l.removeProgram(removeData);
+				break;
+			}
+			//Fragmentation
+			case 3: {
+
+				break;
+			}
+			//Print memory
+			case 4: {
+				l.printList();
+				break;
+			}
+			//Exit
+			case 5: {
+
+				break;
+			}
+		}
+	}while(userChoice != 5);
 
 	return 0;
 }
 
 void programMode(int argc, char* argv[]) {
+	cout << endl;
 
 	if(argc != 2) {
-		cout << "Invalid number of arguments. Exiting program." << endl;
+		cout << "Invalid number of arguments. Valid arguments: best/worst." << endl;
 		exit(EXIT_SUCCESS);
 	}
 	else if(strcmp(argv[1], "worst") == 0) {
@@ -36,7 +91,42 @@ void programMode(int argc, char* argv[]) {
 		cout << "Using best fit algorithm" << endl;
 	}
 	else{
-		cout << "Invalid argument. Valid inputs: best/worst. Exiting" << endl;
+		cout << "Invalid argument. Valid arguments: best/worst." << endl;
 		exit(EXIT_SUCCESS);
 	}
+}
+
+int mainMenu() {
+
+	int inputI = 0;
+	do{
+		cout << "1. Add Program \n2. Kill Program \n3. Fragmentation \n4. Print memory \n5. Exit \n" << endl;
+		inputI = userInputInt();
+
+		if(inputI < 1 || inputI > 5) {
+			cout << "Invalid input." << endl;
+		}
+
+	}while((inputI < 1 || inputI > 5));
+
+	return inputI;
+}
+
+int userInputInt() {
+	string inputS;
+	int inputI = 0;
+	try {
+			cin >> inputS;
+			cout << std::endl;
+
+			//Converts the character array into an int, throwing an exception if it wasn't able to
+			inputI = stoi(inputS,nullptr,0);
+
+		//Catches the exception if thrown by stoi
+		} catch(...) {
+			cin.ignore(100000, '\n');
+			cout << "ERROR: Input not an integer." << endl;
+			return 0;
+		}
+	return inputI;
 }
--- pa2.h
+++ pa2.h
@@ -1,39 +1,46 @@
 //pa2.h
 
 #include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 void programMode(int argc, char* argv[]);
+int mainMenu();
+int userInputInt();
 
 struct node {
 	string data;
 	node* next;
-	bool isEmpty;
 };
 
-
-class List {
+class ProgramList {
 private:
 	node* head;
 	node* curr;
-
+	node* temp;
 public:
-	List();
-	void addNode(string data);
+	ProgramList();
+	void addNode(string addData);
+	void removeProgram(string &removeData);
+	bool addProgram(string addData, int programSize, char* argv[]);
 	void printList();
 };
 
-List::List() {
+ProgramList::ProgramList() {
 	head = NULL;
 	curr = NULL;
+	temp = NULL;
 }
 
-void List::addNode(string data) {
+//Adds a new node to the List
+void ProgramList::addNode(string addData) {
 	node* p = new node;
 	p->next = NULL;
-	p->data = data;
+	p->data = addData;
 
 	if(head != NULL) {
 		curr = head;
@@ -48,7 +55,100 @@ void List::addNode(string data) {
 	}
 }
 
-void List::printList() {
+//Removes a node from the List if it exists in the List
+void ProgramList::removeProgram(string &removeData) {
+	curr = head;
+	bool nodeExist = false;
+	int pages = 0;
+
+	while(curr != NULL) {
+
+		if(curr-> data == removeData) {
+			curr-> data = "Free";
+			nodeExist = true;
+			pages++;
+		}
+		else {
+			curr = curr-> next;
+		}
+
+	}
+
+	if(nodeExist && pages != 0) {
+		cout << "Program " << removeData << " successfully killed, " << pages << " page(s) reclaimed." << endl;
+	}
+	else {
+		cout << "Program " << removeData << " not found." << endl;
+	}
+	curr = NULL;
+
+}
+
+//Replaces a node, finding the place for it using the best or worst fit algotrithm
+bool ProgramList::addProgram(string addData, int pagesRequired, char* argv[]) {
+	curr = head;
+	int pages = 0;
+	int nodeIndex = 0;
+
+	if(strcmp(argv[1], "worst") == 0) {
+			int spaceMax = 0;
+			int nodeIndexMax = 0;
+			while(curr != NULL) {
+				if(curr-> data == addData) {
+					cout << "Error, program " << addData << " already running." << endl;
+					return false;
+				}
+				else if(curr-> data == "Free") {
+					nodeIndex++;
+					pages++;
+				}
+				else if(curr-> data != "Free") {
+					if(pages >= spaceMax) {
+						spaceMax = pages;
+						nodeIndexMax = nodeIndex - pages + 1;
+						pages = 0;
+						nodeIndex++;
+						cout << spaceMax << "S";
+						cout << nodeIndexMax << "I";
+					}
+					else{
+						pages = 0;
+						nodeIndex++;
+					}
+
+				}
+				curr = curr-> next;
+			}
+
+			if(nodeIndexMax > 31) {
+				cout << "Error, not enough available memory for program " << addData << endl;
+				return false;
+			}
+			
+			curr = head;
+			if(spaceMax <= pagesRequired) {
+				for(int i = 0; i < nodeIndexMax; i++) {
+					curr = curr-> next;
+				}
+
+				for(int i = 0; i < pagesRequired; i++) {
+					curr-> data = addData;
+					curr = curr-> next;
+				}
+
+				return true;
+			}
+		return false;
+
+	}
+	else if(strcmp(argv[1], "best") == 0) {
+
+	} 
+
+}
+
+//Prints all data stored in the List
+void ProgramList::printList() {
 	curr = head;
 
 	for(int i = 0; i < 4; i++) {
@@ -58,10 +158,12 @@ void List::printList() {
 				curr = curr-> next;
 			}
 			else {
-				cout << curr-> data << " ";
+				cout << curr-> data << "  ";
 				curr = curr-> next;
 			}
 		}
-		cout << "\n";
+		cout << endl;
 	}
+
+	cout << endl;
 }
